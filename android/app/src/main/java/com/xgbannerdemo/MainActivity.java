a4eeea2ad9d81eb8735ac0e198fcfed49d99915e
vito <developervito@163.com>
2018-06-11 15:56:10 +0800

    测试数据

--- android/app/src/main/java/com/xgbannerdemo/MainActivity.java
+++ android/app/src/main/java/com/xgbannerdemo/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends ReactActivity {
 
     private String[] strings = {"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449518226&di=b0534ce7158ca8e51d3a199d934bdce1&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01e8a157f86d8ca84a0d304fcb9943.jpg%402o.jpg",
             "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449518225&di=b3a4246dd186af19b17f63bf5c86e486&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F018335598924a2a801215603db0836.png%402o.png",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449518225&di=b3a4246dd186af19b17f63bf5c86e486&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F018335598924a2a801215603db0836.png%402o.png",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449518225&di=b3a4246dd186af19b17f63bf5c86e486&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F018335598924a2a801215603db0836.png%402o.png",
+            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449518225&di=b3a4246dd186af19b17f63bf5c86e486&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F018335598924a2a801215603db0836.png%402o.png",
             "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1528449646201&di=f87bfff31d6c2500e7a312f465d8edfe&imgtype=0&src=http%3A%2F%2Fimage.tianjimedia.com%2FuploadImages%2F2014%2F287%2F32%2F1E2205O0EVSB_1000x500.jpg" };
 
 //    @Override
